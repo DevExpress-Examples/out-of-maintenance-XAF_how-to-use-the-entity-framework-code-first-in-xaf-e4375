83149960a401d4afab0303b7bda2289ec50aea5e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ VB/MySolution.Module/Module.vb
@@ -0,0 +1,18 @@
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.ExpressApp.EF
+
+
+Namespace MySolution.Module
+    Public NotInheritable Partial Class MySolutionModule
+        Inherits ModuleBase
+
+        Public Sub New()
+            InitializeComponent()
+            ExportedTypeHelpers.AddExportedTypeHelper(New EFExportedTypeHelperCF())
+        End Sub
+        Public Overrides Function GetModuleUpdaters(ByVal objectSpace As IObjectSpace, ByVal versionFromDB As Version) As IEnumerable(Of ModuleUpdater)
+            Return ModuleUpdater.EmptyModuleUpdaters
+        End Function
+    End Class
+End Namespace
---
+++ VB/MySolution.Module/MyDbContext.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Linq
+Imports System.Data.Entity
+
+Namespace MySolution.Module
+    Public Class MyDbContext
+        Inherits DbContext
+
+        Public Sub New(ByVal connectionString As String)
+            MyBase.New(connectionString)
+        End Sub
+        Private privateEmployees As DbSet(Of Employee)
+        Public Property Employees() As DbSet(Of Employee)
+            Get
+                Return privateEmployees
+            End Get
+            Set(ByVal value As DbSet(Of Employee))
+                privateEmployees = value
+            End Set
+        End Property
+        Private privateTasks As DbSet(Of Task)
+        Public Property Tasks() As DbSet(Of Task)
+            Get
+                Return privateTasks
+            End Get
+            Set(ByVal value As DbSet(Of Task))
+                privateTasks = value
+            End Set
+        End Property
+    End Class
+End Namespace
