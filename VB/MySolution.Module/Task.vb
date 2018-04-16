32900235adbee38f813d3110ebabec7ef7d05246
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ VB/MySolution.Module/Task.vb
@@ -0,0 +1,25 @@
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.Persistent.Base
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.DC
+
+Namespace MySolution.Module
+    <DefaultClassOptions, ImageName("BO_Task")> _
+    Public Class Task
+        Private privateId As Integer
+        <Browsable(False)> _
+        Public Property Id() As Integer
+            Get
+                Return privateId
+            End Get
+            Protected Set(ByVal value As Integer)
+                privateId = value
+            End Set
+        End Property
+        Public Property Subject() As String
+        <FieldSize(FieldSizeAttribute.Unlimited)> _
+        Public Property Description() As String
+        Public Overridable Property AssignedTo() As Employee
+    End Class
+End Namespace
---
+++ VB/MySolution.Module/Updater.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Linq
+Imports System.Text
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.ExpressApp
+
+Namespace MySolution.Module
+    Public Class Updater
+        Inherits ModuleUpdater
+
+        Public Sub New(ByVal objectSpace As IObjectSpace, ByVal currentDBVersion As Version)
+            MyBase.New(objectSpace, currentDBVersion)
+        End Sub
+        Public Overrides Sub UpdateDatabaseAfterUpdateSchema()
+            If ObjectSpace.GetObjects(Of Employee)().Count = 0 Then
+                Dim employee = ObjectSpace.CreateObject(Of Employee)()
+                employee.FirstName = "Mary"
+                employee.LastName = "Tellitson"
+                Dim task = ObjectSpace.CreateObject(Of Task)()
+                task.Subject = "Check reports"
+                task.AssignedTo = employee
+            End If
+            ObjectSpace.CommitChanges()
+        End Sub
+    End Class
+End Namespace
