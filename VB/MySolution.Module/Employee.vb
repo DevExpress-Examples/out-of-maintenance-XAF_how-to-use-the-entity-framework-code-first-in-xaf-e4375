2379c856657ebb2208b15bd4cfcadd8b2547da61
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ VB/MySolution.Module/Employee.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Linq
+Imports System.Text
+Imports DevExpress.Persistent.Base
+Imports System.ComponentModel
+
+Namespace MySolution.Module
+    <DefaultClassOptions, ImageName("BO_Employee")> _
+    Public Class Employee
+        Private privateId As Integer
+        <Browsable(False)> _
+        Public Property Id() As Integer
+            Get
+                Return privateId
+            End Get
+            Protected Set(ByVal value As Integer)
+                privateId = value
+            End Set
+        End Property
+        Public Property FirstName() As String
+        Public Property LastName() As String
+        Public Property Birthday() As Date?
+        Public Overridable Property Tasks() As List(Of Task)
+    End Class
+End Namespace
---
+++ VB/MySolution.Module/Module.vb
@@ -0,0 +1,20 @@
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.ExpressApp.EF
+Imports System.Data.Entity
+
+
+Namespace MySolution.Module
+    Public NotInheritable Partial Class MySolutionModule
+        Inherits ModuleBase
+
+        Public Sub New()
+            InitializeComponent()
+            Database.SetInitializer(Of MyDbContext)(New DropCreateDatabaseIfModelChanges(Of MyDbContext)())
+        End Sub
+        Public Overrides Function GetModuleUpdaters(ByVal objectSpace As IObjectSpace, ByVal versionFromDB As Version) As IEnumerable(Of ModuleUpdater)
+            Dim updater As ModuleUpdater = New Updater(objectSpace, versionFromDB)
+            Return New ModuleUpdater() { updater }
+        End Function
+    End Class
+End Namespace
